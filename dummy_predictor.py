f00905a95eff5390cae025f76eb341852b2e6d4d
Andrey Velichko <andrey4vel@gmail.com>
2023-07-24 21:21:54 +0300

    Added error handling

--- dummy_predictor.py
+++ dummy_predictor.py
@@ -26,7 +26,6 @@ class DummyPredictor:
 
         trade_stream_thread = threading.Thread(target=self.trade_stream_worker)
         trade_stream_thread.start()
-
         self.predictor_worker()
 
     def trade_stream_worker(self):
@@ -36,15 +35,18 @@ class DummyPredictor:
         socket.setsockopt(zmq.SUBSCRIBE, bytes("", "utf-8"))
 
         while True:
-            # b'{"symbol":"BTCUSDT","event_type":"quote_tick","dateMs":1689426049985,"price":27063.2,"qnt":1024,"source":"emulator"}'
-            json_msg = socket.recv()
-            data = json.loads(json_msg)
-            print(json_msg)
+            try:
+                # b'{"symbol":"BTCUSDT","event_type":"quote_tick","dateMs":1689426049985,"price":27063.2,"qnt":1024,"source":"emulator"}'
+                json_msg = socket.recv()
+                data = json.loads(json_msg)
+                print(json_msg)
 
-            if data["event_type"] == "quote_tick":
-                symbol = data["symbol"]
-                price = data["price"]
-                self.quote_tick[symbol] = price  # update last price
+                if data["event_type"] == "quote_tick":
+                    symbol = data["symbol"]
+                    price = data["price"]
+                    self.quote_tick[symbol] = price  # update last price
+            except Exception as e:
+                logging.error(str(e))
 
     def predictor_worker(self):
         logging.warning(f"Starting ZMQ advice publisher: {self.cfg.trade_advice_pub}")
@@ -52,43 +54,50 @@ class DummyPredictor:
         socket.bind(self.cfg.trade_advice_pub)
 
         while True:
-            # At this point should be real ML algorithm
-            # in this code only example how to draw elements on the chart
+            try:
+                json_msg = self.predict()
+                socket.send(bytes(json_msg, "utf-8"))
+                time.sleep(1)
+            except Exception as e:
+                logging.error(str(e))
 
-            predict_hours = 24
-            cur_dt = datetime.now()
-            print(f"Start prediction: {cur_dt}...")
+    def predict(self):
+        # At this point should be real ML algorithm
+        # in this code only example how to draw elements on the chart
 
-            cur_dt0 = cur_dt + timedelta(hours=1)
-            cur_dt1 = cur_dt + timedelta(hours=predict_hours)
+        predict_hours = 24
+        cur_dt = datetime.now()
+        print(f"Start prediction: {cur_dt}...")
 
-            lines = []
-            # draw any lines, for example support levels
-            lines.append(ChartLineDto(cur_dt0, 1.5, cur_dt1, 1.5, "PCT", "#008800"))
-            lines.append(ChartLineDto(cur_dt0, 1.7, cur_dt1, 1.7, "PCT", "#009900"))
-            lines.append(ChartLineDto(cur_dt0, -1.5, cur_dt1, -1.5, "PCT", "#880000"))
-            lines.append(ChartLineDto(cur_dt0, -1.7, cur_dt1, -1.7, "PCT", "#990000"))
+        cur_dt0 = cur_dt + timedelta(hours=1)
+        cur_dt1 = cur_dt + timedelta(hours=predict_hours)
 
-            items = []
-            for ix, hour in enumerate(range(predict_hours)):
-                # predict percent change from current price
-                next_dt = cur_dt + timedelta(hours=hour + 1)
-                predict_pct_up = 0.5 + (ix / 15)
-                items.append(ChartItemDto(next_dt, predict_pct_up, "PCT", "0", 2 + (ix / 3), "#559955"))
+        lines = []
+        # draw any lines, for example support levels
+        lines.append(ChartLineDto(cur_dt0, 1.5, cur_dt1, 1.5, "PCT", "#008800"))
+        lines.append(ChartLineDto(cur_dt0, 1.7, cur_dt1, 1.7, "PCT", "#009900"))
+        lines.append(ChartLineDto(cur_dt0, -1.5, cur_dt1, -1.5, "PCT", "#880000"))
+        lines.append(ChartLineDto(cur_dt0, -1.7, cur_dt1, -1.7, "PCT", "#990000"))
 
-                predict_pct_down = -0.5 - (ix / 15)
-                items.append(ChartItemDto(next_dt, predict_pct_down, "PCT", "0", 2 + (ix / 3), "#995555"))
+        items = []
+        for ix, hour in enumerate(range(predict_hours)):
+            # predict percent change from current price
+            next_dt = cur_dt + timedelta(hours=hour + 1)
+            predict_pct_up = 0.5 + (ix / 15)
+            items.append(ChartItemDto(next_dt, predict_pct_up, "PCT", "0", 2 + (ix / 3), "#559955"))
 
-            last_price = self.quote_tick.get(self.cfg.trade_symbol)
-            if last_price:
-                # draw same image on the chart
-                items.append(ChartItemDto(cur_dt1, 0.5, "PCT", "0", 10, "#559999"))
-                items.append(ChartItemDto(cur_dt1, -0.5, "PCT", "0", 10, "#995599"))
+            predict_pct_down = -0.5 - (ix / 15)
+            items.append(ChartItemDto(next_dt, predict_pct_down, "PCT", "0", 2 + (ix / 3), "#995555"))
 
-            json_obj = {}
-            json_obj["chartLines"] = lines
-            json_obj["chartItems"] = items
+        last_price = self.quote_tick.get(self.cfg.trade_symbol)
+        if last_price:
+            # draw same image on the chart
+            items.append(ChartItemDto(cur_dt1, 0.5, "PCT", "0", 10, "#559999"))
+            items.append(ChartItemDto(cur_dt1, -0.5, "PCT", "0", 10, "#995599"))
 
-            json_msg = json.dumps(json_obj, cls=EnhancedJSONEncoder)
-            socket.send(bytes(json_msg, "utf-8"))
-            time.sleep(1)
+        json_obj = {}
+        json_obj["chartLines"] = lines
+        json_obj["chartItems"] = items
+
+        json_msg = json.dumps(json_obj, cls=EnhancedJSONEncoder)
+        return json_msg
